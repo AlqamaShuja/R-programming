48f96d4bbb2def8e2161f526bb2361ee6915a4e6
AlqamaShuja <alqamashuja313@gmail.com>
2022-10-07 00:43:45 +0500

    basic of R

---
+++ first.R
@@ -0,0 +1,47 @@
+num<-10
+num_2<-20.2
+
+cat(num, " ", num_2)
+name = "Hello"
+print(name)
+
+char = 'ab'
+char1 = 'n'
+
+class(char1)
+class(num_2)
+num_2 = as.integer(num_2)
+class(num_2)
+print(num_2)
+
+vec1<-c(1, 0, 5)
+vec2<-c(1, 4, 5)
+
+print(vec1&vec2)
+
+
+
+
+
+print(123%%10)
+print(as.integer(123/10))
+
+
+
+x<-239L
+if(is.integer(x)){
+  print("X is an integer")
+}else {
+  print("x is not a integer");
+}
+
+
+
+
+
+
+
+
+
+
+
