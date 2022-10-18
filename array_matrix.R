d892ed362e54258838eefa713a08e83ed5d644f0
AlqamaShuja <alqamashuja313@gmail.com>
2022-10-19 00:10:59 +0500

    vect_func & array_matrix

---
+++ array_matrix.R
@@ -0,0 +1,15 @@
+#array
+
+arr = array(c(3, 4, 6, 7,8, 9,1,2, 19), dim = c(3,2,2), dimnames = list(c("r1", "r2", "r3"), c("c1", "c2")))
+
+
+
+
+print(arr)
+#arr[row, col, matrix]
+
+
+
+#Matrix
+mat = matrix(c(2, 3, 4,5, 7, 8, 12, 15, 5), nrow=3, ncol = 3, byrow = FALSE)
+mat
---
+++ vectorFunc.R
@@ -0,0 +1,10 @@
+
+rep(c(2, 3, 4, 6), time=3)
+rep(c(1, 4, 6), each=3)
+rep(c(1, 4, 9), time=c(2, 4, 1))
+rep(1:4, length.out=9)
+
+
+seq(from=2.0, to=5.5, by=0.6)
+seq(from=-2, to=2, length.out=10)
+seq(from=-2, to=2, length.out=10)
